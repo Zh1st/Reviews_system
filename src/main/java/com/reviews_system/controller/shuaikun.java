4bc95431cd08eee025710c94d3590255b1b1dde9
HP <1307508619@qq.com>
2022-05-10 14:49:51 +0800

    测试

--- src/main/java/com/reviews_system/controller/shuaikun.java
+++ src/main/java/com/reviews_system/controller/shuaikun.java
@@ -2,6 +2,6 @@ package com.reviews_system.controller;
 
 public class shuaikun {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello000");
     }
 }
