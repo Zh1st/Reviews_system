67dee35c060b5565cf990f4111063f0d13aeb229
41388 <413888060@qq.com>
2022-05-10 19:49:22 +0800

    王测试

--- src/main/java/com/reviews_system/controller/jaing.java
+++ src/main/java/com/reviews_system/controller/jaing.java
@@ -2,6 +2,6 @@ package com.reviews_system.controller;
 
 public class jaing {
     public static void main(String[] args) {
-        System.out.println("dasifhjaf");
+        System.out.println("蒋帅帅帅帅帅帅帅");
     }
 }
