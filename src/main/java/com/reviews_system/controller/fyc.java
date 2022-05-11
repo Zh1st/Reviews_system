1e341b79f5cb19f81257453eb9b23d23f7e7c673
JZF <1164847231@qq.com>
2022-05-11 13:13:48 +0800

    jzf

--- src/main/java/com/reviews_system/controller/fyc.java
+++ src/main/java/com/reviews_system/controller/fyc.java
@@ -2,6 +2,6 @@ package com.reviews_system.controller;
 
 public class fyc {
     public static void main(String[] args) {
-        System.out.println("fkdsh");
+        System.out.println("15345645634561");
     }
 }
